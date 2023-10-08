a5b2a07711536de13eda71cb53e378053dbcf891
BucBharat <58068566+BucBharat@users.noreply.github.com>
2023-10-08 15:35:57 +0530

    Beautified register screen

--- src/RegisterScreen.js
+++ src/RegisterScreen.js
@@ -7,72 +7,83 @@ import {
   Container,
   Paper,
 } from '@mui/material';
-
+import { useNavigate } from 'react-router-dom';
 function Register() {
   const [userDetails, setUserDetails] = useState({
     name: '',
     email: '',
     password: '',
   });
-
+  const navigate = useNavigate();
   const handleInputChange = event => {
     const { name, value } = event.target;
     setUserDetails(prev => ({ ...prev, [name]: value }));
   };
-
+  const redirectToLogin = () => {
+    navigate('/login');
+  };
   const handleSubmit = event => {
     event.preventDefault();
     // Add registration logic here...
+    navigate('/dashboard');
   };
 
   return (
-    <Container
-      component={Paper}
-      maxWidth="sm"
-      style={{ padding: '20px', marginTop: '20px' }}
-    >
-      <Typography variant="h5" style={{ marginBottom: '20px' }}>
-        Register
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <Box m={2}>
-          <TextField
-            fullWidth
-            name="name"
-            label="Name"
-            variant="outlined"
-            value={userDetails.name}
-            onChange={handleInputChange}
-          />
-        </Box>
-        <Box m={2}>
-          <TextField
-            fullWidth
-            name="email"
-            label="Email"
-            variant="outlined"
-            value={userDetails.email}
-            onChange={handleInputChange}
-          />
-        </Box>
-        <Box m={2}>
-          <TextField
-            fullWidth
-            name="password"
-            label="Password"
-            variant="outlined"
-            type="password"
-            value={userDetails.password}
-            onChange={handleInputChange}
-          />
-        </Box>
-        <Box m={2}>
-          <Button variant="contained" color="primary" type="submit">
-            Register
-          </Button>
-        </Box>
+    <section id="entry-page">
+      <form>
+        <h2>Join Us!</h2>
+        <fieldset>
+          <legend>Register</legend>
+          <ul>
+            <li className="input-row">
+              <label htmlFor="name" className="input-label">
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={userDetails.name}
+                onChange={handleInputChange}
+                required
+              />
+            </li>
+            <li className="input-row">
+              <label htmlFor="email" className="input-label">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={userDetails.email}
+                onChange={handleInputChange}
+                required
+              />
+            </li>
+            <li className="input-row">
+              <label htmlFor="password" className="input-label">
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={userDetails.password}
+                onChange={handleInputChange}
+                required
+              />
+            </li>
+          </ul>
+        </fieldset>
+        <button type="button" onClick={handleSubmit}>
+          Register
+        </button>
+        <button type="button" onClick={redirectToLogin}>
+          Already have an account? Login
+        </button>
       </form>
-    </Container>
+    </section>
   );
 }
 
