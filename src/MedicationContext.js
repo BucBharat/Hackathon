25a6eddb2b7319a11a489c06a4fe0b4b67ba0572
BucBharat <58068566+BucBharat@users.noreply.github.com>
2023-10-09 09:54:13 +0530

    Added comments to Medication context file

--- src/MedicationContext.js
+++ src/MedicationContext.js
@@ -1,16 +1,24 @@
 // MedicationContext.js
 import React from 'react';
 
+// Creating a new context for medications
 const MedicationContext = React.createContext();
 
+// Exporting the context for use in other components
 export default MedicationContext;
+
+// A provider component for the MedicationContext
 export function MedicationProvider({ children }) {
+  // State hook to manage an array of medications
   const [medications, setMedications] = React.useState([]);
 
+  // Function to add a new medication to the medications state
   const addMedication = newMedication => {
     setMedications(prevMedications => [...prevMedications, newMedication]);
   };
 
+  // Wrapping children components with the MedicationContext provider
+  // and passing down the medications state and the addMedication function as values
   return (
     <MedicationContext.Provider value={{ medications, addMedication }}>
       {children}
