284eb58dd874ab59665684b14342b1e0a9f2a963
BucBharat <58068566+BucBharat@users.noreply.github.com>
2023-10-09 09:54:50 +0530

    Added comments for user dashboard

--- src/UserDashboard.js
+++ src/UserDashboard.js
@@ -1,3 +1,4 @@
+// Importing necessary React hooks and Material-UI components
 import React, { useState } from 'react';
 import {
   AppBar,
@@ -12,33 +13,50 @@ import {
   ButtonGroup,
 } from '@mui/material';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+// Custom components for medication management and notifications
 import MedicationInput from './MedicationInput';
 import { Notifs } from './Notifs';
 import { ViewMedications } from './ViewMedications';
+
+// Hook for programmatic navigation
 import { useNavigate } from 'react-router-dom';
 
+// TabPanel functional component to render content based on selected tab
 function TabPanel({ children, value, index }) {
   return value === index ? <Box p={3}>{children}</Box> : null;
 }
 
+// UserDashboard component for displaying the main user interface
 export default function UserDashboard() {
+  // State hooks for tab management and menu anchor
   const [tabValue, setTabValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
+
+  // Hook for programmatic navigation
   const navigate = useNavigate();
+
+  // Handler for changing the tab
   const handleTabChange = (event, newValue) => {
     setTabValue(newValue);
   };
 
+  // Handler for opening the user menu
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
 
+  // Handler for closing the user menu
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  // Handler for logout action
   const handleLogout = () => {
     navigate('/login');
   };
+
+  // Styles for the tabs and app bar
   const selectedTabStyle = {
     backgroundColor: '#769FCD',
     color: 'white',
@@ -62,6 +80,8 @@ export default function UserDashboard() {
     margin: '0 15px',
     boxShadow: '2px 2px 10px rgba(0,0,0,0.2)', // Adding a slight shadow for depth
   };
+
+  // Main render of the component
   return (
     <div style={{ backgroundColor: '#71C9CE' }}>
       <AppBar position="static" style={appBarStyle}>
